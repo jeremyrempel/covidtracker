a1ad6cf67954510b4f4f075ffa36f172b776ec2d
Jeremy Rempel <jr@idreamincolour.com>
2020-08-14 22:44:08 -0700

    update:

--- androidApp/src/main/java/com/jeremyrempel/covidtracker/android/api/MyApi.kt
+++ androidApp/src/main/java/com/jeremyrempel/covidtracker/android/api/MyApi.kt
@@ -2,6 +2,12 @@ package com.jeremyrempel.covidtracker.android.api
 
 import io.ktor.client.HttpClient
 
-class MyApi {
-    val http = HttpClient()
+/**
+ * https://api.covidtracking.com/v1/us/current.json
+ */
+class MyApi(val client: HttpClient = HttpClient()) {
+
+    suspend fun getDataFromNetwork() {
+
+    }
 }
