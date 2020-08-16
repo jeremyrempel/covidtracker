de63761f2c351f9d1268cc2f8891515ae67a01d1
Jeremy Rempel <jr@idreamincolour.com>
2020-08-15 23:35:20 -0700

    call out to network get data

--- androidApp/src/main/java/com/jeremyrempel/covidtracker/android/MainActivity.kt
+++ androidApp/src/main/java/com/jeremyrempel/covidtracker/android/MainActivity.kt
@@ -6,10 +6,20 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.ui.platform.setContent
 import com.jeremyrempel.covidtracker.android.ui.MyApp
 import com.jeremyrempel.covidtracker.android.ui.MyAppTheme
+import com.jeremyrempel.covidtracker.api.MyApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val job = GlobalScope.launch {
+            val api = MyApi()
+            val result = api.getCurrentData()
+            println(result)
+        }.start()
+
         setContent {
             MyAppTheme(isSystemInDarkTheme()) {
                 MyApp()
--- shared/src/commonMain/kotlin/com/jeremyrempel/covidtracker/api/MyApi.kt
+++ shared/src/commonMain/kotlin/com/jeremyrempel/covidtracker/api/MyApi.kt
@@ -1,18 +1,48 @@
 package com.jeremyrempel.covidtracker.api
 
 import io.ktor.client.HttpClient
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.features.json.serializer.KotlinxSerializer.Companion.DefaultJsonConfiguration
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
 import io.ktor.client.request.get
+import kotlinx.serialization.json.Json
 
 /**
  * https://api.covidtracking.com/v1/us/current.json
  */
 class MyApi(
-    private val client: HttpClient = HttpClient(),
     private val scheme: String = "https",
     private val host: String = "api.covidtracking.com"
 
 ) {
-    suspend fun getCurrentData(): ApiResult {
+
+    private val client = HttpClient {
+
+        install(JsonFeature) {
+            val config = Json {
+                isLenient = false
+                ignoreUnknownKeys = true
+                allowSpecialFloatingPointValues = true
+                useArrayPolymorphism = false
+            }
+            serializer = KotlinxSerializer(config)
+        }
+        install(Logging) {
+            logger = object : Logger {
+                override fun log(message: String) {
+                    // todo implement logging
+                    println("Network: $message")
+                }
+            }
+
+            level = LogLevel.INFO
+        }
+    }
+
+    suspend fun getCurrentData(): List<ApiResult> {
         return client.get(
             scheme = scheme,
             host = host,
